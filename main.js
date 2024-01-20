142845d7283fe25a22bbe2e585b32f688b4f2548
Boris Alekseev <a402539@yandex.ru>
2024-01-20 21:40:50 +0300

    1

--- main.js
+++ main.js
@@ -1,41 +1,89 @@
-console.log("The extension is up and running")
+var fio = ['Але','Анд','Бар'];
+var cInd = 0;
+var arr = [];
+var hash = {};
+//var fs = require('fs');
 
-var arr =document.getElementsByClassName("s-code-block")
-
-for(let i = 0 ; i < arr.length ; i++){
- var btn = document.createElement("button")
- btn.classList.add("copy_code_button")
- btn.appendChild(document.createTextNode("Copy"))
- arr[i].appendChild(btn)
- btn.style.position = "relative"
- 
- if(arr[i].scrollWidth === arr[i].offsetWidth && arr[i].scrollHeight === arr[i].offsetHeight)
-  btn.style.left = `${arr[i].offsetWidth - 70}px`
-
-  else if(arr[i].scrollWidth != arr[i].offsetWidth && arr[i].scrollHeight === arr[i].offsetWidth)
-   btn.style.left = `${arr[i].offsetWidth - 200}px`
- else 
-   btn.style.left = `${arr[i].offsetWidth - 150}px`
-
-
- 
- if(arr[i].scrollHeight === arr[i].offsetHeight)
-   btn.style.bottom = `${arr[i].offsetHeight - 50}px`
-   
- else
-   btn.style.bottom = `${arr[i].scrollHeight - 50}px`
-   
-   console.log("Appended")
+function checkTr() {
+	let tbody = window.document.getElementById('search-data');
+	return tbody.children.length;
 }
 
-console.log(arr[0].childNodes[0].innerText)
+function parsePage() {
+	if (cInd <= fio.length) {
+		let tbody = window.document.getElementById('search-data');
+		if (checkTr()) {
+			hash[cInd] = Object.values(tbody.children).map(t => t.innerText);
+			//tbody.innerHTML = '';
+		}
+		else {
+			console.log('test', cInd);
+			if (cInd) {
+				setTimeout(parsePage, 1000);
+				return;
+			}
+		}
 
+		let s = window.document.getElementById('surname');
+		if (s) s.value = fio[cInd++];
+		else alert("не нашли: surname ");
+		
+		let b = window.document.getElementById('search');
+		if (b) {
+			b.click();
+			parsePage();
+		}
 
- var button = document.querySelectorAll(".copy_code_button")
- button.forEach((elm)=>{
-  elm.addEventListener('click',(e)=>{
-    navigator.clipboard.writeText(elm.parentNode.childNodes[0].innerText)
-    alert("Copied to Clipboard")
-  })
- })
+	} else {
+		// arr = Object.values(hash).reduce((a, c) => { a = a.concat(c); return a;}, []);
+		console.log('test', hash, arr);
+		//navigator.clipboard.writeText(JSON.stringify(hash, null, 2));
+		writeJSON(JSON.stringify(hash, null, 2), 'result.json')
+		//console.log(JSON.stringify(hash), JSON.parse(JSON.stringify(hash)));
+	}
+}
 
+/*
+function readTextFile(file, callback) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.overrideMimeType("application/json");
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function() {
+        if (rawFile.readyState === 4 && rawFile.status == "200") {
+            callback(rawFile.responseText);
+        }
+    }
+    rawFile.send(null);
+}
+
+//usage:
+readTextFile("/Users/Documents/workspace/test.json", function(text){
+    var data = JSON.parse(text);
+    console.log(data);
+});
+
+chrome.runtime.getPackageDirectoryEntry(function(root) {
+	root.getFile("g://github/Copy-Code/result.json", {}, function(fileEntry) {
+		fileEntry.file(function(file) {
+			var reader = new FileReader();
+			reader.onloadend = function(e) {
+				var myFile = JSON.parse(this.result);
+				//do here whatever with your JS object(s)
+				console.log(JSON.stringify(myFile, null, 2))
+			};
+			reader.readAsText(file);
+		});
+	});
+});
+*/
+
+function writeJSON(hash, file){
+	var vLink = document.createElement('a'),
+	vBlob = new Blob([hash], {type: "octet/stream"}),
+	vUrl = window.URL.createObjectURL(vBlob);
+	vLink.setAttribute('href', vUrl);
+	vLink.setAttribute('download', file );
+	vLink.click();
+}
+
+window.addEventListener("load", parsePage);
